3b1fda6c492368036943c513847134a22ab86e69
gwoo <gwoohoo@gmail.com>
2012-05-08 18:19:19 -0700

    installed

--- wp-config.php
+++ wp-config.php
@@ -16,22 +16,23 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'press');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '4fa94d4c52e6e');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'ee4c83d8a88520d043fac90498f2292535f3c38a');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'instance13715.db.xeround.com:9251');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
 
 /**#@+
  * Authentication Unique Keys and Salts.
